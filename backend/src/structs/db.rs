1abf4f5e51d975c404b4e50e20608fefc1d60092
Cesium133 <cesium133@MacBook-Pro.local>
2024-12-15 10:45:11 -0500

    add embeddings

--- backend/src/structs/db.rs
+++ backend/src/structs/db.rs
@@ -1,16 +1,19 @@
 use std::sync::Arc;
 
+use fastembed::TextEmbedding;
 use qdrant_client::Qdrant;
 
 #[derive(Clone)]
 pub struct Db {
-    client:Arc<Qdrant>
+    client:Arc<Qdrant>,
+    embeding:Arc<TextEmbedding>,
 }
 
 impl Db {
     pub fn new()->Self{
         Db {
-            client: Arc::new(Qdrant::from_url("http://localhost:6334").build().unwrap())
+            client: Arc::new(Qdrant::from_url("http://localhost:6334").build().unwrap()),
+            embeding: Arc::new(TextEmbedding::try_new(Default::default()).unwrap())
         }
     }
 
